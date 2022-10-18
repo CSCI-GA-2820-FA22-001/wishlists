66ccd475daef359f4af08b815793ead60803f258
Jaikar <jaikar@moengage.com>
2022-10-18 14:55:16 -0400

    added db models and create wishlists route

--- service/models.py
+++ service/models.py
@@ -5,6 +5,7 @@ All of the models are stored in this module
 """
 import logging
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 logger = logging.getLogger("flask.app")
 
@@ -14,27 +15,31 @@ db = SQLAlchemy()
 
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
-
     pass
 
 
-class YourResourceModel(db.Model):
+class Wishlists(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents Wishlists
     """
 
     app = None
 
+
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    name = db.Column(db.String(63), nullable=False)
+    customer_id = db.Column(db.Integer, nullable=False) 
+    created_on = db.Column(db.DateTime, default=datetime.datetime.now())
+    items = db.relationship('Items', backref='wishlists', lazy=True)
+
 
     def __repr__(self):
-        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
+        return "<Wishlist %r id=[%s]>" % (self.name, self.id)
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a wishlist to the database
         """
         logger.info("Creating %s", self.name)
         self.id = None  # id must be none to generate next primary key
@@ -43,19 +48,19 @@ class YourResourceModel(db.Model):
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a wishlist to the database
         """
         logger.info("Saving %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """ Removes a wishlist from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
+        """ Serializes a wishlist into a dictionary """
         return {"id": self.id, "name": self.name}
 
     def deserialize(self, data):
@@ -68,9 +73,7 @@ class YourResourceModel(db.Model):
         try:
             self.name = data["name"]
         except KeyError as error:
-            raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
-            )
+            raise DataValidationError("Invalid Wishlist : missing " + error.args[0])
         except TypeError as error:
             raise DataValidationError(
                 "Invalid YourResourceModel: body of request contained bad or no data - "
@@ -90,22 +93,125 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the YourResourceModels in the database """
+        """ Returns all of the wishlist in the database """
         logger.info("Processing all YourResourceModels")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
+        """ Finds a wishlist by it's ID """
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+        """Returns all wishlists with the given name
 
         Args:
             name (string): the name of the YourResourceModels you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
+
+
+class Items(db.Model):
+    """
+    Class that represents Wishlists
+    """
+
+    app = None
+
+
+    # Table Schema
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63), nullable=False)
+    wishlist_id = db.Column(db.Integer, db.ForeignKey('wishlists.id'),nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    rank =  db.Column(db.Integer, nullable=False)
+    quantity =  db.Column(db.Integer, nullable=False,default=1)
+    price = db.Column(db.Integer, nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_on = db.Column(db.DateTime, default=datetime.datetime.now())
+
+
+    def __repr__(self):
+        return "<Wishlist %r id=[%s]>" % (self.name, self.id)
+
+    def create(self):
+        """
+        Adds a wishlist items to the database
+        """
+        logger.info("Creating %s", self.name)
+        self.id = None  # id must be none to generate next primary key
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Updates a wishlist item to the database
+        """
+        logger.info("Saving %s", self.name)
+        db.session.commit()
+
+    def delete(self):
+        """ Removes a wishlist item from the data store """
+        logger.info("Deleting %s", self.name)
+        db.session.delete(self)
+        db.session.commit()
+
+    def serialize(self):
+        """ Serializes a wishlist item into a dictionary """
+        return {"id": self.id, "name": self.name}
+
+    def deserialize(self, data):
+        """
+        Deserializes a YourResourceModel from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.name = data["name"]
+        except KeyError as error:
+            raise DataValidationError("Invalid Wishlist : missing " + error.args[0])
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid YourResourceModel: body of request contained bad or no data - "
+                "Error message: " + error
+            )
+        return self
+
+    @classmethod
+    def init_db(cls, app):
+        """ Initializes the database session """
+        logger.info("Initializing database")
+        cls.app = app
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
+
+    @classmethod
+    def all(cls):
+        """ Returns all of the wishlist items in the database """
+        logger.info("Processing all YourResourceModels")
+        return cls.query.all()
+
+    @classmethod
+    def find(cls, by_id):
+        """ Finds a wishlist item by it's ID """
+        logger.info("Processing lookup for id %s ...", by_id)
+        return cls.query.get(by_id)
+
+    @classmethod
+    def find_by_name(cls, name):
+        """Returns all wishlists items with the given name
+
+        Args:
+            name (string): the name of the YourResourceModels you want to match
+        """
+        logger.info("Processing name query for %s ...", name)
+        return cls.query.filter(cls.name == name)
+
+    
+
--- service/routes.py
+++ service/routes.py
@@ -6,7 +6,9 @@ Describe what your service does here
 
 from flask import Flask, jsonify, request, url_for, make_response, abort
 from .common import status  # HTTP Status Codes
-from service.models import YourResourceModel
+from service.models import Wishlists,Items
+from flask import request, jsonify
+import json
 
 # Import Flask application
 from . import app
@@ -19,9 +21,26 @@ from . import app
 def index():
     """ Root URL response """
     return (
-        "Reminder: return some useful information in json format about the service here",
+        "Reminder",
         status.HTTP_200_OK,
     )
+
+@app.route("/wishlists", methods=["POST"])
+def create_wishlist():
+    data = json.loads(request.data)
+    try:
+        name = data['name']
+        customer_id = data['customer_id'] 
+        wishlist = Wishlists(name=name,customer_id=customer_id)
+        wishlist.create()
+        return (wishlist.serialize(),
+        status.HTTP_200_OK,
+    )
+
+    except KeyError as e:
+        pass
+
+
 
 
 ######################################################################
@@ -32,4 +51,5 @@ def index():
 def init_db():
     """ Initializes the SQLAlchemy app """
     global app
-    YourResourceModel.init_db(app)
+    Wishlists.init_db(app)
+    Items.init_db(app)
