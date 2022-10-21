509419658e106ad459fa7cce720c9caf772b13e7
Jaikar <jaikar@moengage.com>
2022-10-21 00:42:40 -0400

    added tests and routes for create wishlist

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,48 +1,136 @@
+# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
-TestYourResourceModel API Service Test Suite
+Pet API Service Test Suite
 
 Test cases can be run with the following:
   nosetests -v --with-spec --spec-color
   coverage report -m
+  codecov --token=$CODECOV_TOKEN
+
+  While debugging just these tests it's convenient to use this:
+    nosetests --stop tests/test_service.py:TestPetService
 """
+
 import os
 import logging
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+
+# from unittest.mock import MagicMock, patch
+from urllib.parse import quote_plus
 from service import app
-from service.models import db
-from service.common import status  # HTTP Status Codes
+from service.common import status
+from service.models import db, Wishlists, Items
+from tests.factories import WishlistsFactory, ItemsFactory
+
+# Disable all but critical errors during normal test run
+# uncomment for debugging failing tests
+# logging.disable(logging.CRITICAL)
+
+# DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
+BASE_URL = "/wishlists"
 
 
 ######################################################################
-#  T E S T   C A S E S
+#  T E S T   WISHLIST   S E R V I C E
 ######################################################################
-class TestYourResourceServer(TestCase):
-    """ REST API Server Tests """
+class TestWishlistsService(TestCase):
+    """Wishlist Server Tests"""
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
+        """Run once before all tests"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Wishlists.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
-        pass
+        """Run once after all tests"""
+        db.session.close()
 
     def setUp(self):
-        """ This runs before each test """
-        self.app = app.test_client()
+        """Runs before each test"""
+        self.client = app.test_client()
+        db.session.query(Wishlists).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
-        """ This runs after each test """
-        pass
+        db.session.remove()
+
+    def _create_pets(self, count):
+        """Factory method to create wishlists in bulk"""
+        pets = []
+        for _ in range(count):
+            test_wishlists = WishlistsFactory()
+            response = self.client.post(BASE_URL, json=test_wishlists.serialize())
+            self.assertEqual(
+                response.status_code, status.HTTP_201_CREATED, "Could not create test wishlist"
+            )
+            new_wishlist = response.get_json()
+            test_wishlists.id = new_wishlist["id"]
+            pets.append(new_wishlist)
+        return pets
 
     ######################################################################
-    #  P L A C E   T E S T   C A S E S   H E R E
+    #  T E S T   C A S E S
     ######################################################################
 
     def test_index(self):
-        """ It should call the home page """
-        resp = self.app.get("/")
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        """It should call the Home Page"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], "Wishlists Demo REST API Service")
+
+    def test_health(self):
+        """It should be healthy"""
+        response = self.client.get("/healthcheck")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["status"], 200)
+        self.assertEqual(data["message"], "Healthy")
+
+    def test_create_wishlist(self):
+        """It should Create a new wishlist"""
+        test_wishlist = WishlistsFactory()
+        logging.debug("Test Wishlist: %s", test_wishlist.serialize())
+        response = self.client.post(BASE_URL, json=test_wishlist.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = response.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_wishlist = response.get_json()
+        self.assertEqual(new_wishlist["name"], test_wishlist.name)
+        self.assertEqual(new_wishlist["customer_id"], test_wishlist.customer_id)
+
+        # Check that the location header was correct
+        response = self.client.get(location)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_wishlist = response.get_json()
+        self.assertEqual(new_wishlist["name"], test_wishlist.name)
+        self.assertEqual(new_wishlist["customer_id"], test_wishlist.customer_id)
+
+
+
