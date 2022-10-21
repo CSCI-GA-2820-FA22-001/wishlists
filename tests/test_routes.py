69ee08e05c645a4921a71ffe20a88f74284d40fb
Jaikar <jaikar@moengage.com>
2022-10-21 01:22:32 -0400

    Added tests and routes for adding item to wishlist

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -134,3 +134,103 @@ class TestWishlistsService(TestCase):
 
 
 
+######################################################################
+#  T E S T   ITEMS   S E R V I C E
+######################################################################
+class TestItemsService(TestCase):
+    """Items Server Tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Run once before all tests"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Items.init_db(app)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Run once after all tests"""
+        db.session.close()
+
+    def setUp(self):
+        """Runs before each test"""
+        self.client = app.test_client()
+        db.session.query(Items).delete()  # clean up the last tests
+        db.session.query(Wishlists).delete()  # clean up the last wishlists
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+
+    def _create_items(self, count):
+        """Factory method to create items in bulk"""
+        items = []
+        for _ in range(count):
+            test_wishlist = WishlistsFactory()
+            test_wishlist.id = None
+            test_wishlist.create()
+            test_item = ItemsFactory()
+            test_item.wishlist_id =test_wishlist.id
+            response = self.client.post(BASE_URL, json=test_item.serialize())
+            self.assertEqual(
+                response.status_code, status.HTTP_201_CREATED, "Could not create test item"
+            )
+            new_item = response.get_json()
+            test_item.id = new_item["id"]
+            items.append(new_item)
+        return items
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+
+    def test_index(self):
+        """It should call the Home Page"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], "Wishlists Demo REST API Service")
+
+    def test_health(self):
+        """It should be healthy"""
+        response = self.client.get("/healthcheck")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["status"], 200)
+        self.assertEqual(data["message"], "Healthy")
+
+    def test_add_item_to_wishlist(self):
+        """It should Create a new ITEM"""
+        test_item = ItemsFactory()
+        logging.debug("Test Item: %s", test_item.serialize())
+        test_wishlist = WishlistsFactory()
+        test_wishlist.id = None
+        test_wishlist.create()
+        test_item = ItemsFactory()
+        test_item.wishlist_id = test_wishlist.id
+        URL = BASE_URL + "/" + str(test_wishlist.id) + "/items"
+        response = self.client.post(URL, json=test_item.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = response.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_item = response.get_json()
+        self.assertEqual(new_item["name"], test_item.name)
+        self.assertEqual(new_item["product_id"], test_item.product_id)
+
+        # Check that the location header was correct
+        response = self.client.get(location)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_item = response.get_json()
+        self.assertEqual(new_item["name"], test_item.name)
+        self.assertEqual(new_item["product_id"], test_item.product_id)
+
+
+
+
