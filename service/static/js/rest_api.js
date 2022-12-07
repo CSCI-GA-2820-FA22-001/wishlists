1f4bbd41ce62a892b63467a3e06b24b57c1e0d25
sarvanin <sarvani.minty@gmail.com>
2022-12-07 18:47:00 -0500

    Added a UI page and changed 2 TDD tests
    Added a UI page and changed 2 TDD tests.
    Added css files and js
    
    Added installation files for selenium etc.
    Added Vagrant file

---
+++ service/static/js/rest_api.js
@@ -0,0 +1,253 @@
+$(function () {
+
+    // ****************************************
+    //  U T I L I T Y   F U N C T I O N S
+    // ****************************************
+
+    // Updates the form with data from the response
+    function update_form_data(res) {
+        $("#pet_id").val(res.id);
+        $("#pet_name").val(res.name);
+        $("#pet_category").val(res.category);
+        if (res.available == true) {
+            $("#pet_available").val("true");
+        } else {
+            $("#pet_available").val("false");
+        }
+        $("#pet_gender").val(res.gender);
+        $("#pet_birthday").val(res.birthday);
+    }
+
+    /// Clears all form fields
+    function clear_form_data() {
+        $("#wishlist_name").val("");
+        $("#wishlist_id").val("");
+        $("#created_on").val("");
+        $("#item_id").val("");
+        $("#item_name").val("");
+        $("#product_id").val("");
+        $("#item_quantity").val("");
+        $("#item_price").val("");
+    }
+
+    // Updates the flash message area
+    function flash_message(message) {
+        $("#flash_message").empty();
+        $("#flash_message").append(message);
+    }
+
+    // ****************************************
+    // Create a Wishlist
+    // ****************************************
+
+    $("#create-btn").click(function () {
+
+        let id = $("#wishlist_id").val();
+        let name = $("#wishlist_name").val();
+        let customer_id = $("#customer_id").val();
+        let created_on = $("#created_on").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+        
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/pets",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+
+    // ****************************************
+    // Update a Pet
+    // ****************************************
+
+    $("#update-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+        let name = $("#pet_name").val();
+        let category = $("#pet_category").val();
+        let available = $("#pet_available").val() == "true";
+        let gender = $("#pet_gender").val();
+        let birthday = $("#pet_birthday").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+                type: "PUT",
+                url: `/pets/${pet_id}`,
+                contentType: "application/json",
+                data: JSON.stringify(data)
+            })
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Retrieve a Pet
+    // ****************************************
+
+    $("#retrieve-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/pets/${pet_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a Pet
+    // ****************************************
+
+    $("#delete-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "DELETE",
+            url: `/pets/${pet_id}`,
+            contentType: "application/json",
+            data: '',
+        })
+
+        ajax.done(function(res){
+            clear_form_data()
+            flash_message("Pet has been Deleted!")
+        });
+
+        ajax.fail(function(res){
+            flash_message("Server error!")
+        });
+    });
+
+    // ****************************************
+    // Clear the form
+    // ****************************************
+
+    $("#clear-btn").click(function () {
+        $("#pet_id").val("");
+        $("#flash_message").empty();
+        clear_form_data()
+    });
+
+    // ****************************************
+    // Search for a Wishlist
+    // ****************************************
+
+    $("#search-btn").click(function () {
+
+        let id = $("#wishlist_id").val();
+        let customer_id = $("#customer_id").val();
+        let available = $("#pet_available").val() == "true";
+
+        let queryString = ""
+        
+        if (id) {
+           get_url = `/wishlists/${id}`
+        }
+        else if (customer_id){
+            
+            queryString += 'category=' + category
+            get_url = `/wishlists?${queryString}`
+        }
+        else {
+            get_url = `/wishlists`
+        }
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: get_url,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-2">ID</th>'
+            table += '<th class="col-md-2">Name</th>'
+            table += '<th class="col-md-2">Customer ID</th>'
+            table += '<th class="col-md-2">Created On</th>'
+            table += '</tr></thead><tbody>'
+            let firstWList = "";
+            for(let i = 0; i < res.length; i++) {
+                let wishlist = res[i];
+                table +=  `<tr id="row_${i}"><td>${wishlist.id}</td><td>${wishlist.name}</td><td>${wishlist.customer_id}</td><td>${wishlist.created_on}</td></tr>`;
+                if (i == 0) {
+                    firstWList = wishlist;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_results").append(table);
+
+            // copy the first result to the form
+            /*if (firstPet != "") {
+                update_form_data(firstPet)
+            }*/
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+})
